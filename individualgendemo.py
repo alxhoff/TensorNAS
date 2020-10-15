c832271e92d40be1569209d736d15c53f96c80d8
Alex Hoffman <alxhoff@gmail.com>
2020-10-15 21:03:26 +0200

    Added prints to demo

--- individualgendemo.py
+++ individualgendemo.py
@@ -22,51 +22,58 @@ images_test /= 255
 mnist_class_count = 10
 #######
 
-# test = Conv2D.Layer(input_tensor_shape)
-# print(test.get_name())
-# test.print()
-# test.mutate()
-#
-# test = Flatten.Layer(input_tensor_shape)
-# print(test.get_name())
-# test.print()
-# test.mutate()
-#
-# test = Dropout.Layer(input_tensor_shape)
-# print(test.get_name())
-# test.print()
-# test.mutate()
-#
-# test = HiddenDense.Layer(input_tensor_shape)
-# print(test.get_name())
-# test.print()
-# test.mutate()
-# test.print()
-#
-# test = OutputDense.Layer(input_tensor_shape, 10)
-# print(test.get_name())
-# test.print()
-# test.mutate()
-# test.print()
-#
-# test = Reshape.Layer(input_tensor_shape, [1, 784])
-# print(test.get_name())
-# test.print()
-# test.mutate()
-# test.print()
-#
-# test = MaxPool2D.Layer(input_tensor_shape)
-# print(test.get_name())
-# test.print()
-# test.mutate()
-# test.print()
-#
-# test = MaxPool3D.Layer(input_tensor_shape)
-# print(test.get_name())
-# test.print()
-# test.mutate()
-# test.print()
+from tensornas.layers import Conv2D, Reshape, Dropout, Flatten
+from tensornas.layers.Dense import HiddenDense, OutputDense
+from tensornas.layers.MaxPool import MaxPool3D, MaxPool2D
 
+print("Unit testing layers")
+
+test = Conv2D.Layer(input_tensor_shape)
+print(test.get_name())
+test.print()
+test.mutate()
+
+test = Flatten.Layer(input_tensor_shape)
+print(test.get_name())
+test.print()
+test.mutate()
+
+test = Dropout.Layer(input_tensor_shape)
+print(test.get_name())
+test.print()
+test.mutate()
+
+test = HiddenDense.Layer(input_tensor_shape)
+print(test.get_name())
+test.print()
+test.mutate()
+test.print()
+
+test = OutputDense.Layer(input_tensor_shape, 10)
+print(test.get_name())
+test.print()
+test.mutate()
+test.print()
+
+test = Reshape.Layer(input_tensor_shape, [1, 784])
+print(test.get_name())
+test.print()
+test.mutate()
+test.print()
+
+test = MaxPool2D.Layer(input_tensor_shape)
+print(test.get_name())
+test.print()
+test.mutate()
+test.print()
+
+test = MaxPool3D.Layer(input_tensor_shape)
+print(test.get_name())
+test.print()
+test.mutate()
+test.print()
+
+print("Testing classification block architecture")
 
 model = classificationblockarchitectures.ClassificationBlockArchitecture(
     input_tensor_shape, mnist_class_count
@@ -86,6 +93,4 @@ model.evaluate(
     metrics=["accuracy"],
 )
 
-# # idea, constrain previous layer, it stop multiple flatten being created sequentially
-#
 print("Done")
