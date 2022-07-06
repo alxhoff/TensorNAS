dbd5a9be363ffe99cf8f22d11c0086f86db559ca
Alex Hoffman <alxhoff@gmail.com>
2022-07-06 13:35:05 +0200

    Added pool size mutation to pool layers

--- Layers/Pool/__init__.py
+++ Layers/Pool/__init__.py
@@ -1,6 +1,6 @@
 from TensorNAS.Core import EnumWithNone
 from TensorNAS.Core.Layer import Layer
-from TensorNAS.Core.LayerMutations import MutateStrides, MutatePadding
+from TensorNAS.Core.LayerMutations import MutateStrides, MutatePadding, MutatePoolSize
 from enum import auto
 
 
@@ -22,6 +22,6 @@ class Args(EnumWithNone):
     PADDING = auto()
 
 
-class Layer(Layer, MutateStrides, MutatePadding):
+class Layer(Layer, MutateStrides, MutatePadding, MutatePoolSize):
     MAX_POOL_SIZE = 7
     MAX_STRIDE = 7
