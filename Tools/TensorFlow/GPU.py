659cf9453db1248145dafc66cddd6473aee2cfa4
Alex Hoffman <alxhoff@gmail.com>
2024-03-27 10:51:59 +0100

    Toggled GPU memory growth off for now

--- Tools/TensorFlow/GPU.py
+++ Tools/TensorFlow/GPU.py
@@ -1,9 +1,9 @@
 def config_GPU():
     import tensorflow as tf
 
-    gpus = tf.config.experimental.list_physical_devices("GPU")
-    for device in gpus:
-        tf.config.experimental.set_memory_growth(device, True)
+    # gpus = tf.config.experimental.list_physical_devices("GPU")
+    # for device in gpus:
+    #     tf.config.experimental.set_memory_growth(device, True)
 
     # config = tf.compat.v1.ConfigProto()
     # config.gpu_options.allow_growth = True
